99898b10da3c58fa4ca54fd37c4393ffd3ff475a
Victor Golovin <rinoally@gmail.com>
2023-06-13 21:34:38 +1000

    finale code refactoring

--- js/index.js
+++ js/index.js
@@ -1,28 +1,37 @@
 const STATUS_IN_LIMIT = "Все хорошо";
 const STATUS_OUT_OF_LIMIT = "Все плохо";
+const STATUS_IN_LIMIT_TEXT = "stats-status-text-positive";
+const STATUS_OUT_OF_LIMIT_TEXT = "stats-status-text-negative";
+const AMOUNT_OF_EXPENSES_TEXT = "Введите сумму трат!";
+const CATEGORY = "Категория";
+const SELECT_CATEGORY_TEXT = "Выберете категорию!";
+const ENTER_THE_LIMIT_TEXT = "Введите лимит!";
+const CLICK_SEND_TEXT = "Нажмите отправить!";
 const CHANGE_LIMIT_TEXT = "Новый лимит";
 const CURRENCY = "руб.";
+const POPUP_OPENED_CLASSNAME = "expenses-popup-open";
+const BODY_FIXED_CLASSNAME = "expenses-body-fixed";
 
-const inputNode = document.getElementById("expenseInput");
-const categorySelectNode = document.getElementById("categorySelect");
-const addButtonNode = document.getElementById("addButton");
-const clearBunntonNode = document.getElementById("clearButton");
-const totalValueNode = document.getElementById("totalValue");
-const statusNode = document.getElementById("statusText");
-const historyList = document.getElementById("historyList");
+
+const inputNode = document.getElementById("expense-input");
+const categorySelectNode = document.getElementById("category-select");
+const addButtonNode = document.getElementById("add-button");
+const clearBunntonNode = document.getElementById("clear-button");
+const totalValueNode = document.getElementById("total-value");
+const statusNode = document.getElementById("status-text");
+const historyList = document.getElementById("history-list");
 const changeLimitBtn = document.getElementById("popup-inner-btn");
 const validationMessage = document.getElementById("expensesValidation-message");
 const popupInputNode = document.getElementById("expenses-popup-input");
 const popupValidationMessage = document.getElementById("popup-expensesValidation-message");
 
-const POPUP_OPENED_CLASSNAME = "expenses-popup-open";
-const BODY_FIXED_CLASSNAME = "expenses-body-fixed";
 
 const bodyNode = document.getElementById("body");
 const popupNode = document.getElementById("expenses-popup");
 const btnOpenNode = document.getElementById("popup-changeLimitBtn");
 const popupContentNode = document.getElementById("expenses-popup");
 const btnCloseNode = document.getElementById("popup-close-btn");
+
 
 const limitNode = document.getElementById("limitValue");
 let limit = parseInt(limitNode.innerText);
@@ -44,10 +53,10 @@ const renderStatus = () => {
 
   if (total <= limit) {
     statusNode.innerText = STATUS_IN_LIMIT;
-    statusNode.className = "stats-statusText-positive";
+    statusNode.className = STATUS_IN_LIMIT_TEXT;
   } else {
-    statusNode.innerText = `${STATUS_OUT_OF_LIMIT} (${limit - total} руб)`;
-    statusNode.className = "stats-statusText-negative";
+    statusNode.innerText = `${STATUS_OUT_OF_LIMIT} (${limit - total} ${CURRENCY})`;
+    statusNode.className = STATUS_OUT_OF_LIMIT_TEXT;
   }
 };
 
@@ -81,13 +90,13 @@ const clearInput = (input) => {
 
 const validation = () => {
   if (!inputNode.value) {
-    validationMessage.innerText = `Введите сумму трат!`;
+    validationMessage.innerText = AMOUNT_OF_EXPENSES_TEXT;
     validationMessage.classList.remove("expenses-validation-message-hidden");
     return;
   }
 
-  if (categorySelectNode.value === "Категория") {
-    validationMessage.innerText = `Выберете категорию!`;
+  if (categorySelectNode.value === CATEGORY) {
+    validationMessage.innerText = SELECT_CATEGORY_TEXT;
     validationMessage.classList.remove("expenses-validation-message-hidden");
     return;
   }
@@ -97,13 +106,13 @@ const validation = () => {
 
 const popupValidation = () => {
   if (!popupInputNode.value) {
-    popupValidationMessage.innerText = `Введите новый лимит!`;
+    popupValidationMessage.innerText = ENTER_THE_LIMIT_TEXT;
     popupValidationMessage.classList.remove("popup-expensesValidation-message-hidden");
     return;
   }
 
   if (popupInputNode.value) {
-    popupValidationMessage.innerText = `Нажмите отправить!`;
+    popupValidationMessage.innerText = CLICK_SEND_TEXT;
     popupValidationMessage.classList.remove("popup-expensesValidation-message-hidden");
     return;
   }
@@ -130,7 +139,7 @@ const addButtonHandler = () => {
   }
 
   const currentCategory = getSelectedCategory();
-  if (currentCategory === "Категория") {
+  if (currentCategory === CATEGORY) {
     return;
   }
 
@@ -152,14 +161,13 @@ const changeLimitHandler = () => {
   const newLimitValue = parseInt(newLimit);
 
   if (!newLimitValue) {
-    "Введите лимит!"
     return;
   }
 
   limitNode.innerText = `${newLimitValue} ${CURRENCY}`;
   limit = newLimitValue;
 
-  expensesTogglePopup();
+  expensesTogglePopupHandler();
   render();
 };
 
@@ -170,19 +178,19 @@ popupNode.addEventListener("click", (event) => {
     .includes(popupContentNode);
 
   if (isClickOutsideContent) {
-    expensesTogglePopup();
+    expensesTogglePopupHandler();
   }
 
   clearInput(popupInputNode);
   popupValidation();
 });
 
-const expensesTogglePopup = () => {
+const expensesTogglePopupHandler = () => {
   popupNode.classList.toggle(POPUP_OPENED_CLASSNAME);
 };
 
-btnOpenNode.addEventListener("click", expensesTogglePopup);
-btnCloseNode.addEventListener("click", expensesTogglePopup);
+btnOpenNode.addEventListener("click", expensesTogglePopupHandler);
+btnCloseNode.addEventListener("click", expensesTogglePopupHandler);
 addButtonNode.addEventListener("click", addButtonHandler);
 clearBunntonNode.addEventListener("click", clearButtonHandler);
 changeLimitBtn.addEventListener("click", changeLimitHandler);
